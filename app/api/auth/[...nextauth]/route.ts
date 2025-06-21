09c654b15113ba30315f310a79c97ecf36797f59
SolomonJS <janserik405@outlook.com>
2025-06-21 15:41:44 +0500

    Some features

--- app/api/auth/[...nextauth]/route.ts
+++ app/api/auth/[...nextauth]/route.ts
@@ -1,7 +1,22 @@
 import NextAuth from "next-auth";
+import {PrismaAdapter} from "@auth/prisma-adapter";
+import CredentialsProvider from "next-auth/providers/credentials";
+import prisma from "@/libs/prismadb";
 
 const handler = NextAuth({
-
+    adapter: PrismaAdapter(prisma),
+    providers: [
+        CredentialsProvider({
+            name: "credential",
+            credentials: {
+                email: { label: "email", type: "text" },
+                password: { label: "password", type: "password"}
+            },
+            async authorize(credentials) {
+                
+            }
+        })
+    ]
 })
 
 export {
