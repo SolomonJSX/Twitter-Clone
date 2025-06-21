d3ca240a4640f76385f637c183a153e035974246
SolomonJS <janserik405@outlook.com>
2025-06-21 20:11:23 +0500

    Application was provided prisma jwt authentication

--- app/api/auth/[...nextauth]/route.ts
+++ app/api/auth/[...nextauth]/route.ts
@@ -1,7 +1,8 @@
 import NextAuth from "next-auth";
-import {PrismaAdapter} from "@auth/prisma-adapter";
+import { PrismaAdapter } from "@auth/prisma-adapter";
 import CredentialsProvider from "next-auth/providers/credentials";
 import prisma from "@/libs/prismadb";
+import bcrypt from "bcrypt";
 
 const handler = NextAuth({
     adapter: PrismaAdapter(prisma),
@@ -10,13 +11,51 @@ const handler = NextAuth({
             name: "credential",
             credentials: {
                 email: { label: "email", type: "text" },
-                password: { label: "password", type: "password"}
+                password: { label: "password", type: "password" }
             },
             async authorize(credentials) {
-                
+                if (!credentials?.email || !credentials?.password) throw new Error("Invalid credentials")
+
+                const user = await prisma.user.findUnique({
+                    where: {
+                        email: credentials.email
+                    }
+                })
+
+                if (!user || !user?.hashedPassword) throw new Error("Invalid credentials")
+
+                const isCorrectPassword = await bcrypt.compare(credentials.password, user.hashedPassword)
+
+                if (!isCorrectPassword) throw new Error("Invalid credentials")
+
+                return {
+                    ...user,
+                    id: user.id.toString()
+                }
             }
         })
-    ]
+    ],
+    debug: process.env.NODE_ENV === "development",
+    session: {
+        strategy: "jwt"
+    },
+    callbacks: {
+        jwt({token, user}) {
+            if (user) {
+                token.email = user.email
+                token.name = user.name
+            }
+            return token
+        },
+        session({session, token}) {
+            if (token && session.user) {
+                session.user.email = token.email
+                session.user.name = token.name
+            }
+            return session
+        }
+    },
+    secret: process.env.NEXTAUTH_SECRET
 })
 
 export {
